392c4228f2957a534226cbba20dacb4e1841511b
lb1programmer <luke@linuxpc>
2018-04-23 00:53:16 -0700

    This does detect some isolated sounds!

--- 2016-07-37_qt_audio_monitor/go.py
+++ 2016-07-37_qt_audio_monitor/go.py
@@ -1,10 +1,17 @@
+from __future__ import division
 from PyQt4 import QtGui,QtCore
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
+
 
 import sys
 import ui_main
 import numpy as np
 import pyqtgraph
 import SWHear
+import time
+from PyQt4.QtCore import QObject, SIGNAL
 
 class ExampleApp(QtGui.QMainWindow, ui_main.Ui_MainWindow):
     def __init__(self, parent=None):
@@ -16,7 +23,63 @@ class ExampleApp(QtGui.QMainWindow, ui_main.Ui_MainWindow):
         self.maxFFT=0
         self.maxPCM=0
         self.ear = SWHear.SWHear(rate=44100,updatesPerSecond=20)
+        
+        self.progressBar.setVisible(False)
+        QObject.connect(self.LearnSound, SIGNAL("clicked()"), self.learnSound)
+        QObject.connect(self.LearnNotSound, SIGNAL("clicked()"), self.learnNotSound)
+        QObject.connect(self.Train, SIGNAL("clicked()"), self.trainAction)
         self.ear.stream_start()
+        #print dir(self.progressBar)
+        #KEEP learning
+        self.learnArrs = []
+        self.learnNotArrs = []
+        self.clf = None
+        
+    def learnSound(self, *args):
+        print('max:%s' %( np.max(self.ear.fft),))
+        #The scaler works per column. adjust:
+        fft = scaler.fit_transform(self.ear.fft.reshape(self.ear.fft.shape[0],1))
+        fft = fft.reshape(self.ear.fft.shape[0]);
+        print('max:%s' %( np.max(fft),))
+        self.learnArrs.append(fft)
+        self.showInfo()
+        #csv = '\n'.join( [str(x) for x in self.ear.fft] )
+        #with open('out.csv','w') as outputfile:
+        #    outputfile.write(csv)
+        
+    def learnNotSound(self):
+        fft = scaler.fit_transform(self.ear.fft.reshape(self.ear.fft.shape[0],1))
+        fft = fft.reshape(self.ear.fft.shape[0]);
+        self.learnNotArrs.append(fft)
+        self.showInfo();
+
+    def showInfo(self):
+        self.status.setText('%s learn, %s noise' % (len(self.learnArrs), len(self.learnNotArrs)))
+
+    def trainAction(self):
+        self.progressBar.setVisible(True)
+        import numpy
+        from sklearn.svm import SVC
+        clf = SVC(kernel='linear', C=3)
+        #Prep data to fit...
+        data = self.learnArrs + self.learnNotArrs
+        category = [1]*len(self.learnArrs) + [-1]*len(self.learnNotArrs)
+        clf.fit(data, category)
+                
+        #Get m coefficients:
+        coef = clf.coef_[0]
+        b = clf.intercept_[0]
+        self.progressBar.setVisible(False)
+        print('This is the M*X+b=0 equation...')
+        print('M=%s' % (coef))
+        print('b=%s' % (b))
+        print('positives:')
+        for p in self.learnArrs:
+            print clf.predict(p)
+        print('negatives:')
+        for p in self.learnNotArrs:
+            print clf.predict(p)
+        self.clf = clf
 
     def update(self):
         if not self.ear.data is None and not self.ear.fft is None:
@@ -32,7 +95,31 @@ class ExampleApp(QtGui.QMainWindow, ui_main.Ui_MainWindow):
             pen=pyqtgraph.mkPen(color='b')
             self.grPCM.plot(self.ear.datax,self.ear.data,pen=pen,clear=True)
             pen=pyqtgraph.mkPen(color='r')
+            #print(self.ear.fftx.shape)
+            #for i in range(self.ear.fftx.shape[0]):
+            #    print(self.ear.fftx[i])
+            # ^ that is just increments of 20.
+            #print(self.ear.fft.shape)
+            runsum = 0
+            spike = 0
+            spikept = 0
+            for i in range(self.ear.fft.shape[0]):
+                if self.ear.fftx[i] % 100 == 0:
+                    runsum+= self.ear.fft[i]
+                    #print(str(self.ear.fftx[i])+' : '+str(runsum))
+                    if( runsum > spike ):
+                        spikept = self.ear.fftx[i]
+                        spike = runsum
+                    runsum = 0
+                else:
+                    runsum += self.ear.fft[i]
+            if self.clf:
+                fft = scaler.fit_transform(self.ear.fft.reshape(self.ear.fft.shape[0],1))
+                fft = fft.reshape(self.ear.fft.shape[0])#back to simple array
+                print('match: '+str(self.clf.predict(fft)))
+            #print('spike near '+str(spikept))
             self.grFFT.plot(self.ear.fftx,self.ear.fft/self.maxFFT,pen=pen,clear=True)
+            #time.sleep(13000)
         QtCore.QTimer.singleShot(1, self.update) # QUICKLY repeat
 
 if __name__=="__main__":
--- 2016-07-37_qt_audio_monitor/ui_main.py
+++ 2016-07-37_qt_audio_monitor/ui_main.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ui_main.ui'
+# Form implementation generated from reading ui file './ui_main.ui'
 #
-# Created by: PyQt4 UI code generator 4.11.4
+# Created: Tue Apr 17 00:10:43 2018
+#      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
-#from PyQt5 import QtCore, QtGui
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -39,10 +39,17 @@ class Ui_MainWindow(object):
         self.pbLevel.setObjectName(_fromUtf8("pbLevel"))
         self.horizontalLayout.addWidget(self.pbLevel)
         self.frame = QtGui.QFrame(self.centralwidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.frame.sizePolicy().hasHeightForWidth())
+        self.frame.setSizePolicy(sizePolicy)
         self.frame.setFrameShape(QtGui.QFrame.NoFrame)
         self.frame.setFrameShadow(QtGui.QFrame.Plain)
         self.frame.setObjectName(_fromUtf8("frame"))
         self.verticalLayout = QtGui.QVBoxLayout(self.frame)
+        self.verticalLayout.setSpacing(1)
+        self.verticalLayout.setSizeConstraint(QtGui.QLayout.SetMaximumSize)
         self.verticalLayout.setMargin(0)
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
         self.label = QtGui.QLabel(self.frame)
@@ -58,6 +65,57 @@ class Ui_MainWindow(object):
         self.grPCM.setObjectName(_fromUtf8("grPCM"))
         self.verticalLayout.addWidget(self.grPCM)
         self.horizontalLayout.addWidget(self.frame)
+        self.verticalLayout_2 = QtGui.QVBoxLayout()
+        self.verticalLayout_2.setSpacing(0)
+        self.verticalLayout_2.setSizeConstraint(QtGui.QLayout.SetMinimumSize)
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.status = QtGui.QLabel(self.centralwidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.status.sizePolicy().hasHeightForWidth())
+        self.status.setSizePolicy(sizePolicy)
+        self.status.setObjectName(_fromUtf8("status"))
+        self.verticalLayout_2.addWidget(self.status)
+        self.LearnSound = QtGui.QPushButton(self.centralwidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.LearnSound.sizePolicy().hasHeightForWidth())
+        self.LearnSound.setSizePolicy(sizePolicy)
+        self.LearnSound.setObjectName(_fromUtf8("LearnSound"))
+        self.verticalLayout_2.addWidget(self.LearnSound)
+        self.LearnNotSound = QtGui.QPushButton(self.centralwidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.LearnNotSound.sizePolicy().hasHeightForWidth())
+        self.LearnNotSound.setSizePolicy(sizePolicy)
+        self.LearnNotSound.setObjectName(_fromUtf8("LearnNotSound"))
+        self.verticalLayout_2.addWidget(self.LearnNotSound)
+        self.Train = QtGui.QPushButton(self.centralwidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.Train.sizePolicy().hasHeightForWidth())
+        self.Train.setSizePolicy(sizePolicy)
+        self.Train.setObjectName(_fromUtf8("Train"))
+        self.verticalLayout_2.addWidget(self.Train)
+        self.progressBar = QtGui.QProgressBar(self.centralwidget)
+        self.progressBar.setEnabled(True)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.progressBar.sizePolicy().hasHeightForWidth())
+        self.progressBar.setSizePolicy(sizePolicy)
+        self.progressBar.setMaximum(0)
+        self.progressBar.setProperty("value", -1)
+        self.progressBar.setObjectName(_fromUtf8("progressBar"))
+        self.verticalLayout_2.addWidget(self.progressBar)
+        spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout_2.addItem(spacerItem)
+        self.verticalLayout_2.setStretch(3, 1)
+        self.horizontalLayout.addLayout(self.verticalLayout_2)
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -67,5 +125,19 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
         self.label.setText(_translate("MainWindow", "frequency data (FFT):", None))
         self.label_2.setText(_translate("MainWindow", "raw data (PCM):", None))
+        self.status.setText(_translate("MainWindow", "Ready", None))
+        self.LearnSound.setText(_translate("MainWindow", "Learn Sound", None))
+        self.LearnNotSound.setText(_translate("MainWindow", "NOT the Sound", None))
+        self.Train.setText(_translate("MainWindow", "Train SVM", None))
 
 from pyqtgraph import PlotWidget
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    MainWindow = QtGui.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
